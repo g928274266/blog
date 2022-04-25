653e6454543f5672d3b5c6cd901eacce9bf84759
youlixiang <928274266@qq.com>
2022-04-25 21:37:20 +0800

    Oss BUG修改

--- blog-site/src/main/java/com/youlixiang/blog/controller/OssController.java
+++ blog-site/src/main/java/com/youlixiang/blog/controller/OssController.java
@@ -2,12 +2,18 @@ package com.youlixiang.blog.controller;
 
 import com.youlixiang.blog.exception.CustomException;
 import com.youlixiang.blog.service.OssService;
+import com.youlixiang.blog.util.CheckLoginUtils;
 import com.youlixiang.blog.util.CommonResult;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * <p>
@@ -35,7 +41,8 @@ public class OssController {
      */
     @PostMapping("/uploadOssFile")
     @ApiOperation(value = "上传文件")
-    public CommonResult uploadOssFile(MultipartFile multipartFile) throws CustomException {
+    public CommonResult uploadOssFile(HttpServletRequest request, MultipartFile multipartFile) throws CustomException {
+        CheckLoginUtils.isLogin(request);
         String url = ossService.uploadOssFile(multipartFile);
         return CommonResult.success().put("url", url);
     }
@@ -45,10 +52,12 @@ public class OssController {
      *
      * @param fileName 文件名
      * @return 通用返回
+     * @throws CustomException 异常
      */
     @PostMapping("/deleteOssFile")
     @ApiOperation(value = "删除文件")
-    public CommonResult deleteOssFile(@RequestParam("fileName") String fileName) {
+    public CommonResult deleteOssFile(HttpServletRequest request, @RequestParam("fileName") String fileName) throws CustomException {
+        CheckLoginUtils.isLogin(request);
         ossService.deleteOssFile(fileName);
         return CommonResult.success();
     }
